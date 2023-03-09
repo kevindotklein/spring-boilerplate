13ae8ac74997396fcb360dbb2d106c5806ed73ed
kevindotklein <kevindotklein@gmail.com>
2023-03-09 17:08:28 -0300

    create project

---
+++ src/test/java/com/kevindotklein/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevindotklein.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
